12e191e01a4abd021530d65f121c111eeb5f5999
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-19 18:21:36 -0300

    07-its javascript
    07-its javascript

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,15 @@
-const Header = () => (
+const Header = () => {
+
+    const getTheYear = () => {
+        const newDate= new Date();
+        return newDate.getFullYear()
+    }
+    
+    return(
         <div>
-            Header
+            The date is {5+5}
         </div>
-);
+    )
+}
     
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
 import Header from './components/header';
-import NewsList from './components/news_list';
+// import NewsList from './components/news_list';
 
 const App = () => {
     return (
         <>
             <Header/>
-            <NewsList/>
+            {/* <NewsList/> */}
         </>
     )
 }

0e67bf8928ea805f3abc2af6628bda00752aa56f
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-19 18:42:25 -0300

    08-Components history
    08-Components history

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,9 @@
 const Header = () => {
-
-    const getTheYear = () => {
-        const newDate= new Date();
-        return newDate.getFullYear()
-    }
-    
     return(
-        <div>
-            The date is {5+5}
-        </div>
+        <header>
+            <div>LOGO</div>
+            <input/>
+        </header>
     )
 }
     

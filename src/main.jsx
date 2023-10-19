01c019a9af7244e5b6748f569d8ba06595f901a1
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-19 17:09:26 -0300

    04-JSX Behind the scenes
    04-JSX Behind the scenes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
 
-const App = () => (
-    <div>
-        <h1>Hello</h1>
-    </div>
-)
+const App = () => {
+    return (
+        <>
+            <h1>Hello world</h1>
+            <h1>Hello world</h1>
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);

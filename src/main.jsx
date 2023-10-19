6643b52f880005ed13905bf88b0a59e8ae48bcc4
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-19 16:55:20 -0300

    From the ground up
    03-From the ground up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const App = () => (
+    <div>
+        <h1>Hello</h1>
+    </div>
 )
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);

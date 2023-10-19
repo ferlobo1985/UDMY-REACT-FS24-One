dedc06c4a227664cd8e3ecbc12469b3d29992a6f
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-19 19:39:52 -0300

    10-Events
    10-Events

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,21 @@
 const Header = () => {
+
+    const onChangeHandler = (event) => {
+        console.log(event)
+    }
+
     return(
         <header>
-            <div className="logo">LOGO</div>
-            <input/>
+            <div 
+                className="logo"
+                onClick={()=>console.log('I was clicked')}
+                onPointerEnter={e => console.log('onPointerEnter')}
+            >LOGO</div>
+            <input
+                onChange={onChangeHandler}
+                onFocus={e => console.log('on focus')}
+                onBlur={e => console.log('on blur')}
+            />
         </header>
     )
 }
